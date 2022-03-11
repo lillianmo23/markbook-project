76074edae58906aa31fd2efd4f41b0d4c4e92883
augustinso <60696742+augustinso@users.noreply.github.com>
2022-03-11 12:43:17 -0500

    Update add_grades.py

--- mvp-code/add_grades.py
+++ mvp-code/add_grades.py
@@ -4,17 +4,24 @@ import json
 
 with open("students.json", "r") as f:
     grades = json.loads(f.read())
-    
+
 num_of_students = int(input("how many students would you like to add?"))
+
+
 for i in range(num_of_students):
     names = input(f"What is student {i+1}'s name?")
-    print()
+    # grades[names] = None
+
+
     mark = int(input(f"What is {names}'s average?"))
     print()
     
     grades[names] = mark
 
+
+
+
 print(grades)
 
 with open("students.json", "w") as f:
-    json.dump(grades, f, indent = 4)
+    json.dump(grades, f, indent = 4) 
