e6fb9aff07977f22a6613f340d337f3ed57fa4d9
augustinso <60696742+augustinso@users.noreply.github.com>
2022-03-11 12:52:19 -0500

    Update basic-interface.py

--- mvp-code/basic-interface.py
+++ mvp-code/basic-interface.py
@@ -10,6 +10,32 @@ def view_grades():
     
     Include a menu at the bottom to add grades, edit grades, or go back
     """
+    from unicodedata import name
+
+    import json
+
+    with open("students.json", "r") as f:
+        grades = json.loads(f.read())
+
+    num_of_students = int(input("how many students would you like to add?"))
+
+
+    for i in range(num_of_students):
+        names = input(f"What is student {i+1}'s name?")
+        # grades[names] = None
+
+
+        mark = int(input(f"What is {names}'s average?"))
+        print()
+
+        grades[names] = mark
+
+
+
+        print(grades)
+
+with open("students.json", "w") as f:
+    json.dump(grades, f, indent = 4) 
     print("View Grades function")
 
 def view_students():
