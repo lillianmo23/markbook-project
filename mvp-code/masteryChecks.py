292819577fd41d936889b28a77cf2de7250e12a1
JackW-01 <60620735+JackW-01@users.noreply.github.com>
2022-03-11 11:48:49 -0500

    Update masteryChecks.py

--- mvp-code/masteryChecks.py
+++ mvp-code/masteryChecks.py
@@ -1,13 +1,17 @@
-from unicodedata import name
 from typing import List
-import json
 
-def add_mastery_check():
-    """Adds a new mastery check with a selected name"""
-    global check_name
+def add_mastery_check(mastery_checks: List[str]) -> List[str]:
+    """Adds a new mastery check
+    
+    Args:
+        mastery_checks: The initial list of mastery checks
+    
+    Returns:
+        Updated mastery_checks with a new mastery check
+    """
     check_name = input("What is this mastery check called?")
-    with open("students.json", "a") as f:
-        json.dump(check_name, f)
+    return mastery_checks.append(check_name)
+    
 
 def del_checks(mastery_checks: List[str], student_checks: dict) -> List[str]:
     """Removes a mastery check from the list of checks, and moves students to their new mastery check
