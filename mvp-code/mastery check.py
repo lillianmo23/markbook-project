28d09dc07b081fca49b2f08bf452a4de1ca41c8f
JackW-01 <60620735+JackW-01@users.noreply.github.com>
2022-03-11 11:21:24 -0500

    Update mastery check.py

--- mvp-code/mastery check.py
+++ mvp-code/mastery check.py
@@ -3,27 +3,30 @@ from typing import List
 import json
 
 def add_mastery_check():
+    """Adds a new mastery check with a selected name"""
     global check_name
     check_name = input("What is this mastery check called?")
     with open("students.json", "a") as f:
         json.dump(check_name, f)
 
-def del_checks(mastery_checks: List[str]) -> List[str]:
-    """Removes a mastery check from the list of checks
+def del_checks(mastery_checks: List[str], student_checks: dict) -> List[str]:
+    """Removes a mastery check from the list of checks, and moves students to their new mastery check
     
     Args:
         mastery_checks: The list of mastery checks
+        student_checks: The list of checks students are on
     
     Returns:
         An updated list with the chosen check removed
+        An updated dictionary of student information
     """
     while True:
         print("What mastery check would you like to delete?")
         choice = input("> ")
-        try:
-            mastery_checks.remove(choice)
-            break
-        except:
-            input("Invalid Input.\nPress ENTER to continue.")
-    return mastery_checks
-    
+        index = mastery_checks.index(choice)
+        mastery_checks.remove(choice)
+        for key, value in student_checks.items():
+            if value > index + 1:
+                student_checks[key] = value - 1
+        break
+    return mastery_checks, student_checks
